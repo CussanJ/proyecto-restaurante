4e4756ca4c5802456f9659c7c5fb866357f4454e
Jennifer <jenniferstgo22@gmail.com>
2026-05-01 19:06:36 -0600

    estructura base inventario service

---
+++ backend/inventario-service/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/inventario_db')
+    .then(() => console.log('MongoDB inventario conectado'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Servicio inventario funcionando');
+});
+
+app.listen(3002, () => {
+    console.log('Servidor inventario en puerto 3002');
+});
