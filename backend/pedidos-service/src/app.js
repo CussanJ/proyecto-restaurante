a35852a78f024a397bcbeab57e4b200a35051a5a
Jennifer <jenniferstgo22@gmail.com>
2026-05-01 19:39:34 -0600

    estructura base pedidos service

---
+++ backend/pedidos-service/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/pedidos_db')
+    .then(() => console.log('MongoDB pedidos conectado'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Servicio pedidos funcionando');
+});
+
+app.listen(3003, () => {
+    console.log('Servidor pedidos en puerto 3003');
+});
