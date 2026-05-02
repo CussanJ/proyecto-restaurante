301f6c770739d9156d5696559cedb29bd035c4a7
Jennifer <jenniferstgo22@gmail.com>
2026-05-01 20:00:59 -0600

    conexion basica pedidos con inventario

--- backend/inventario-service/src/controllers/inventarioController.js
+++ backend/inventario-service/src/controllers/inventarioController.js
@@ -21,7 +21,31 @@ const obtenerInventario = async (req, res) => {
     }
 };
 
+const actualizarStock = async (req, res) => {
+    try {
+        const { productoId, cantidad } = req.body;
+
+        const item = await Inventario.findOne({ productoId });
+
+        if (!item) {
+            return res.status(404).json({ mensaje: "Producto no encontrado en inventario" });
+        }
+
+        if (item.stock < cantidad) {
+            return res.status(400).json({ mensaje: "Stock insuficiente" });
+        }
+
+        item.stock -= cantidad;
+        await item.save();
+
+        res.json({ mensaje: "Stock actualizado", item });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
 module.exports = {
     crearInventario,
-    obtenerInventario
+    obtenerInventario,
+    actualizarStock
 };
--- backend/pedidos-service/src/app.js
+++ backend/pedidos-service/src/app.js
@@ -1,3 +1,4 @@
+const pedidoRoutes = require('./routes/pedidoRoutes');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/pedidos', pedidoRoutes);
 
 mongoose.connect('mongodb://127.0.0.1:27017/pedidos_db')
     .then(() => console.log('MongoDB pedidos conectado'))
