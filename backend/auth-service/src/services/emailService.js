3ab58fcc1fa3a78476b21081ccdd155e83e88296
Raul <usuarioq_01@proton.me>
2026-05-30 13:54:56 -0600

    add dcker compose for prod

--- backend/auth-service/src/services/emailService.js
+++ backend/auth-service/src/services/emailService.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: process.env.EMAIL_SERVICE || 'gmail',
+  port: process.env.EMAIL_PORT || 25,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
